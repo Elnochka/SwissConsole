f14f44ce7db0e5fa5d16da7c099516b2ed20ba2a
Elena <eleka@ukr.net>
2018-05-13 22:28:26 +0300

    br

--- src/main/java/Swiss.java
+++ src/main/java/Swiss.java
@@ -1,7 +1,4 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,107 +8,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-public class Swiss extends JFrame {
+public class Swiss {
+    private static List<String> listStrings = new ArrayList<String>();
+    private static List<ListString> listListStrings = new ArrayList<ListString>();
 
-    private JTextField fileName = new JTextField(25);
-    private JButton save = new JButton("Select file *.txt");
-    private static Swiss ssp;
-
-
-    public Swiss(){
-        super("Split file by line");
-        setLayout(new FlowLayout());
-        save.addActionListener(new Swiss.SaveInDB());
-        add(save);
-        fileName.setEditable(false);
-        add(fileName);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(350,100);
-        setVisible(true);
-    }
-
-    class SaveInDB implements ActionListener {
-
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser c = new JFileChooser();
-            int rVal = c.showSaveDialog(Swiss.this);
-            if (rVal == JFileChooser.APPROVE_OPTION){
-                fileName.setText(c.getCurrentDirectory().toString() + "\\" + c.getSelectedFile().getName());
-                if(!fileName.getText().endsWith(".txt"))
-                    fileName.setText(c.getCurrentDirectory().toString() + "\\" + c.getSelectedFile().getName() + ".txt");
-
-                try {
-//                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName.getText()), "cp1251"));//"UTF-8"
-//                    FileInputStream fin = new FileInputStream(fileName.getText());
-//                    int numAvailable = fin.available();
-//                    int numByte = 100;
-//
-//                    byte[] buffer;
-//
-//                    while (numAvailable > 0){
-//                        if (numAvailable < numByte){
-//                            buffer = new byte[numAvailable];
-//                        }
-//                        else {
-//                            buffer = new byte[numByte];
-//                        }
-//                        // считываем буфер
-//                        fin.read(buffer, fin.available() - numAvailable, buffer.length);
-//                        StringBuilder stringBuffer = new StringBuilder();
-//                        for (byte byteBuffer : buffer){
-//                            char alphabet = (char) byteBuffer;
-//                            stringBuffer.append(alphabet);
-//                        }
-//                        stringList.add(stringBuffer.toString());
-//                        numAvailable = numAvailable - numByte;
-//                    }
-//                    fin.close();
-
-                    List<String> stringList = new ArrayList<String>();
-                    BufferedReader reader = new BufferedReader(new FileReader(fileName.getText()));
-
-                    while (reader.ready()) {
-                        stringList.add(reader.readLine());
-                    }
-                    reader.close();
-
-
-
-                    try {
-                        getJDBC(fileName.getText(), stringList);
-                    }catch (SQLException ex){
-                        JOptionPane.showMessageDialog(null, "Error SQL!","Error",
-                                JOptionPane.ERROR_MESSAGE);
-                        ex.printStackTrace();
-                    }
-                    JOptionPane.showMessageDialog(null, "File processed!" + " File name " + fileName.getText(),"Hello",
-                            JOptionPane.PLAIN_MESSAGE);
-                } catch (FileNotFoundException ex) {
-                    JOptionPane.showMessageDialog(null, "File not found!","Error",
-                            JOptionPane.ERROR_MESSAGE);
-                    ex.printStackTrace();
-                } catch (UnsupportedEncodingException ex) {
-                    JOptionPane.showMessageDialog(null, "Unsupported encoding!","Error",
-                            JOptionPane.ERROR_MESSAGE);
-                    ex.printStackTrace();
-                } catch (IOException ex){
-                    JOptionPane.showMessageDialog(null, "Error!","Error",
-                            JOptionPane.ERROR_MESSAGE);
-                }
-            }
-            if (rVal == JFileChooser.CANCEL_OPTION){
-                fileName.setText("You pressed cancel!");
-            }
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        Swiss swiss = new Swiss();
+        String fileName = ""; if(args.length == 0 ){
+            System.out.println("You should enter file name in command line");
+            System.exit(0);
+        }else{
+            fileName = args[0];
         }
+
+        swiss.getListStrings(fileName);
+        swiss.getJDBC();
     }
 
-    public List<Object> staticStrings(String[] wordString, int lengthString) {
+    public List<String> getListStrings(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        try {
+
+            while (reader.ready()) {
+                String stringFile = reader.readLine();
+                listStrings.add(stringFile);
+                getListListStrings(stringFile);
+            }
+
+        }catch (IOException ex) {
+            ex.printStackTrace();
+
+        }
+        reader.close();
+        return listStrings;
+    }
+
+    public List<ListString> getListListStrings(String stringFile){
+        ListString listString = new ListString();
+
+        String[] wordString = stringFile.split(" ");
+
         String maxWord = wordString[0];
         String minWord = wordString[0];
         int kolWord = 0;
         int lengthWord = 0;
         for (int i = 0; i < wordString.length; i++) {
-
             if(wordString[i].length() > maxWord.length()){
                 maxWord = wordString[i];
             }
@@ -121,18 +62,24 @@ public class Swiss extends JFrame {
             kolWord = kolWord + 1;
             lengthWord = lengthWord + wordString[i].length();
         }
-
         double avrWord = lengthWord * 1.0 / kolWord * 1.0;
 
-        List<Object> stat = new ArrayList<Object>();
-        stat.add(maxWord);
-        stat.add(minWord);
-        stat.add(lengthString);
-        stat.add(avrWord);
-        return stat;
+        listString.setLongest(maxWord);
+        listString.setAverage(avrWord);
+        listString.setShortest(minWord);
+        listString.setLength(stringFile.length());
+        listListStrings.add(listString);
+
+        return listListStrings;
     }
 
-    public void getJDBC(String fileName, List<String> stringList) throws IOException, SQLException {
+    public void getJDBC(){
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "1234");
+        properties.setProperty("useSSL", "false");
+        properties.setProperty("autoReconnect", "true");
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -141,12 +88,6 @@ public class Swiss extends JFrame {
         }
         Connection connection = null;
 
-        Properties properties = new Properties();
-        properties.setProperty("user", "root");
-        properties.setProperty("password", "1234");
-        properties.setProperty("useSSL", "false");
-        properties.setProperty("autoReconnect", "true");
-
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/h2",properties);
 
@@ -154,43 +95,38 @@ public class Swiss extends JFrame {
             e.printStackTrace();
             return;
         }
-
-        String insertDB = "insert into strings(name) values (?)";
+        String insertStrings = "insert into strings(name) values (?)";
         String insertStatistic = "insert into statistic(longest, shortest,length, average) values (?,?,?,?)";
-        PreparedStatement psDB = null;
-        PreparedStatement psStatistic = null;
+        PreparedStatement preparedStatement = null;
 
-        for (String stringFile : stringList) {
-            if (stringFile.length() < 2) {
-                continue;
+        try {
+            preparedStatement = connection.prepareStatement(insertStrings);
+            for (String stringVar : listStrings) {
+                preparedStatement.setString(1, stringVar);
+                preparedStatement.executeUpdate();
             }
-            String[] wordString = stringFile.split(" ");
-            List<Object> statistic = staticStrings(wordString, stringFile.length());
-            connection.setAutoCommit(false);
-            psStatistic = connection.prepareStatement(insertStatistic);
-            psStatistic.setString(1, (String) statistic.get(0));
-            psStatistic.setString(2, (String) statistic.get(1));
-            psStatistic.setInt(3, (Integer) statistic.get(2));
-            psStatistic.setDouble(4, (Double) statistic.get(3));
-            psStatistic.executeUpdate();
-            psDB = connection.prepareStatement(insertDB);
-            psDB.setString(1, stringFile);
-            psDB.executeUpdate();
-            connection.commit();
+            preparedStatement = connection.prepareStatement(insertStatistic);
+            for (ListString stringVar : listListStrings) {
+                preparedStatement.setString(1, stringVar.getLongest());
+                preparedStatement.setString(2, stringVar.getShortest());
+                preparedStatement.setInt(3, stringVar.getLength());
+                preparedStatement.setDouble(4, stringVar.getAverage());
+                preparedStatement.executeUpdate();
+            }
+            preparedStatement.close();
+            connection.close();
 
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
-        psDB.close();
-        psStatistic.close();
 
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-
-            public void run() {
-                ssp = new Swiss();
-
-            }
-        });
-    }
 }
--- src/test/java/SwissTest.java
+++ src/test/java/SwissTest.java
@@ -2,14 +2,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by Elena on 10.05.2018.
- */
 public class SwissTest {
     private Swiss swiss;
     @Before
@@ -17,19 +15,23 @@ public class SwissTest {
 
     @Test
     public void statistic() {
-        String str = "averagee word length";
-        String[] mas = str.split(" ");
-        List<Object> lst = new LinkedList<Object>();
-        lst.add("averagee");
-        lst.add("word");
-        lst.add(str.length());
-        lst.add(6.0);
-        Assert.assertEquals(swiss.staticStrings(mas, str.length()), lst);
-        List<Object> testString = swiss.staticStrings(mas, str.length());
-        assertTrue((Integer)testString.get(2) == 20);
-        Assert.assertEquals((String) testString.get(1), "word");
-        Assert.assertEquals((String) testString.get(0), "averagee");
-        assertTrue((Double)testString.get(3) == 6.0);
+        String strings = "averagee word length";
+        String[] arrayStrings = strings.split(" ");
+        ListString listString = new ListString();
+        listString.setAverage(6.0);
+        listString.setShortest("word");
+        listString.setLength(20);
+        listString.setLongest("averagee");
+        List<ListString> testString1 = new ArrayList<ListString>();
+        testString1.add(listString);
+//        Assert.assertEquals(swiss.getListListStrings(strings),testString1);
+        List<ListString> testString = swiss.getListListStrings(strings);
+        for(ListString listString1 : testString) {
+            assertTrue(listString1.getLength() == 20);
+            Assert.assertEquals(listString1.getShortest(), "word");
+            Assert.assertEquals(listString1.getLongest(), "averagee");
+            assertTrue(listString1.getAverage() == 6.0);
+        }
     }
 
 }
